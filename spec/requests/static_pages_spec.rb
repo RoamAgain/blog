2214d31b39b372dd5febd07ee768be8fbfecc11d
Akash Chauhan <akash6190@gmail.com>
2012-06-22 20:18:52 +0530

    Change old style hash to new style hash

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,18 @@ describe "Static Pages" do
     it "should have the h1  'RubyGate Blog'" do
 
       visit '/static_pages/home'
-      page.should have_selector 'h1', 
+      page.should have_selector 'h1',
                     :text=>'RubyGate Blog'
     end
-    
-    it "should have right title" do
+    it "should have base title" do
       visit '/static_pages/home'
       page.should have_selector 'title',
-                    :text=>"RubyGate Blog | Home"
+                    :text=>"RubyGate Blog"
+    end
+    it "should have custom title" do
+      visit '/static_pages/home'
+      page.should_not have_selector 'title',
+                    :text=>"| Home"
     end
   end
   describe "Help Page" do
@@ -20,14 +24,13 @@ describe "Static Pages" do
       visit '/static_pages/help'
       page.should have_selector 'h1',:text=>'Help'
     end
-    
-    
+
     it "should have right title" do
       visit '/static_pages/help'
       page.should have_selector 'title',
                     :text=>"RubyGate Blog | Help"
     end
-    
+
   end
 
   describe "About Page" do
@@ -35,27 +38,25 @@ describe "Static Pages" do
       visit '/static_pages/about'
       page.should have_selector 'h1',:text=>'About Us'
     end
-    
-    
+
     it "should have right title" do
       visit '/static_pages/about'
       page.should have_selector 'title',
                     :text=>"RubyGate Blog | About Us"
     end
-    
+
   end
   describe "Contact page" do
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector 'h1',:text=>'Contact'
     end
-    
-    
+
     it "should have right title" do
       visit '/static_pages/contact'
       page.should have_selector 'title',
                     :text=>"RubyGate Blog | Contact"
     end
-    
+
   end
 end
