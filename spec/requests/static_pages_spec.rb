e86ee9d102c7f8f7080b9cc09e9fd01cbb920c2d
Akash Chauhan <akash6190@gmail.com>
2012-06-23 04:01:01 +0530

    Add utilities in spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,62 +1,41 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  subject { page }
+
   describe "Home Page" do
-    it "should have the h1  'RubyGate Blog'" do
 
-      visit root_path
-      page.should have_selector 'h1',
-                    text: 'RubyGate Blog'
-    end
-    it "should have base title" do
-      visit root_path
-      page.should have_selector 'title',
-                    text: "RubyGate Blog"
-    end
-    it "should have custom title" do
-      visit root_path
-      page.should_not have_selector 'title',
-                    text: "| Home"
-    end
+    before { visit root_path }
+
+    it { should have_selector 'h1', text: 'RubyGate Blog'}
+    it { should have_selector 'title', text: full_title('')}
+    it { should_not have_selector 'title', text: "| Home" }
+
   end
-  describe "Help Page" do
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector 'h1',text: 'Help'
-    end
 
-    it "should have right title" do
-      visit help_path
-      page.should have_selector 'title',
-                    text: "RubyGate Blog | Help"
-    end
+  describe "Help Page" do
+
+    before { visit help_path }
+    
+    it {should have_selector 'h1',text: 'Help'}
+    it {should have_selector 'title',text: full_title('Help') }
 
   end
 
   describe "About Page" do
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector 'h1',text: 'About Us'
-    end
 
-    it "should have right title" do
-      visit about_path
-      page.should have_selector 'title',
-                    text: "RubyGate Blog | About Us"
-    end
+    before { visit about_path }
+    
+    it {should have_selector 'h1',text: 'About Us'}
+    it {should have_selector 'title', text: full_title('About Us')}
 
   end
   describe "Contact page" do
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector 'h1',text: 'Contact'
-    end
 
-    it "should have right title" do
-      visit contact_path
-      page.should have_selector 'title',
-                    text: "RubyGate Blog | Contact"
-    end
+    before { visit contact_path }
 
+    it { should have_selector 'h1',text: 'Contact'}
+    it {should have_selector 'title',text: full_title('Contact')}
   end
 end
