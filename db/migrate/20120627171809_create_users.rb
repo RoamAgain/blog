45a505eef01e2db99fff3de1f4d2e1d405a5eb01
Akash Chauhan <akash6190@gmail.com>
2012-06-27 22:51:36 +0530

    Add user migration

---
+++ db/migrate/20120627171809_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      #t.timestamps adds created_at and updated_at
+      t.timestamps
+      
+    end
+  end
+end
