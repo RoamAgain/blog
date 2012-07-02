a7093acb770c3b27611780e5aa9f6ad091a170a7
Akash Chauhan <akash6190@gmail.com>
2012-07-03 02:22:31 +0530

    usermodel added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  # the attributes in the table editable from the web form should be present in the list
   attr_accessible :name,:email
   
-  validates :name,presence: true
+  #validates that name is not blank and the max length is 50
+  validates :name,presence: true, length: {maximum: 50}
+  
+  #validates that email is non blank
+  validates :email,presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,37 @@ describe User do
   
   it{should be_valid }
   
-  describe "when ame is not present" do
+  describe "when name is not present" do
     before { @user.name = " "}
     it { should_not be_valid }
+  end
+  
+  describe "when email is not present" do
+    before { @user.email = " "}
+    it {should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @user.name = 'a'*51}
+    it {should_not be_valid}
+  end
+  
+  describe "when email is invalid" do
+    it "should be invalid" do
+      address = %w[user@foo,com user_at_foo.org example.user@foo. ]
+      address.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+  describe "when email format is valid" do
+    it "should be valid" do
+      address = %w[user@foo.com A_USE@f.b.org user_a@b.foo.org example.user@foo.jp a+b@baz.cn ]
+      address.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
   end
 end
