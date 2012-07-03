5350ef88edb8c40e34ba875d381ed855cdd2d7c7
Akash Chauhan <akash6190@gmail.com>
2012-07-03 18:13:40 +0530

    Add has_secure_passwod tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it {should respond_to :password_digest }
   it {should respond_to :password } # has secured password creates this in memory
   it {should respond_to :password_confirmation }
+  it {should respond_to :authenticate }
   
   it{should be_valid }
   
@@ -79,5 +80,22 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5}
     
     it {should_not be_valid}
+  end
+  
+  describe "return value of authenticate method" do
+    before {@user.save}
+    
+    let(:found_user) {User.find_by_email(@user.email)}
+    
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password)}
+    end
+    
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate('invalid') }
+      
+      it { should_not == user_for_invalid_password}
+      specify { user_for_invalid_password.should be_false}
+    end
   end
 end
