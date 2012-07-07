63ce34d62f1471ad7dea4624e603253cfe460d90
Akash Chauhan <akash6190@gmail.com>
2012-07-07 13:47:49 +0530

    Add gravatar support

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  
+  #Returns the gravatar link for the email address
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
