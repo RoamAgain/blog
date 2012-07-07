ea3319da9219cc9721a92d1bdb6e63a391de88c2
Akash Chauhan <akash6190@gmail.com>
2012-07-07 14:19:24 +0530

    Add signup funtionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-  def new
-  end
-  
   def show
     # raise params.inspect
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save 
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
